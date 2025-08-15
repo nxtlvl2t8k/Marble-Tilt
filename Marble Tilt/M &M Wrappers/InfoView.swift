7869a4ba46fd716d56fb2669e6531a7ef8a130f0
Scott Mayhew <mayhews@hotmail.com>
2025-08-15 01:02:59 -0600

    final working files
    working files. will publish after a couple tests

--- Marble Tilt/M &M Wrappers/InfoView.swift
+++ Marble Tilt/M &M Wrappers/InfoView.swift
@@ -9,22 +9,14 @@
 import SwiftUI
 import MessageUI
 
-//struct AppInfo: Identifiable {
-//    let id = UUID()
-//    let name: String
-//    let url: URL
-//}
-
 struct InfoView: View {
     @Environment(\.dismiss) var dismiss
-//    @State private var selectedApp: AppInfo? = nil
-//    @State private var showingRandomApp = false
     
     @State private var showingMail = false
     @State private var showingMessage = false
     @State private var mailResult: Result<MFMailComposeResult, Error>? = nil
     @State private var messageResult: MessageComposeResult? = nil
-//    @State private var showActivityView = false
+    @State private var showActivityView = false
     
     private let shareText = """
     Check out Marble Tilt App
@@ -32,41 +24,39 @@ struct InfoView: View {
     https://apps.apple.com/ca/app/marble-tilt/id1599878471?mt=8
     """
     
-//    private let apps: [AppInfo] = [
-//         AppInfo(name: "Marble Tilt", url: URL(string: "https://apps.apple.com/ca/app/marble-tilt/id1599878471?mt=8")!),
-//         AppInfo(name: "WinterLand Scratcher", url: URL(string: "https://apps.apple.com/ca/app/winterland-scratcher/id1054185804?mt=8")!),
-//         AppInfo(name: "Itripoley", url: URL(string: "https://apps.apple.com/ca/app/itripoley/id1586541611?mt=8")!)
-//     ]
+    private let shareURL = URL(string: "https://apps.apple.com/ca/developer/huge-holdings-ltd/id508584861?mt=8")!
+    private let developerEmail = "hugeholdings@hotmail.com"
+    private let mailSubject = "Check out 'Marble Tilt' on App Store!"
+    private let bugMailSubject = "Marble Tilt App Report"
 
-//    private let shareURL = URL(string: "https://apps.apple.com/ca/developer/huge-holdings-ltd/id508584861?mt=8")!
-//    private let developerEmail = "hugeholdings@hotmail.com"
-    
-    private let shareURL = URL(string: "http://itunes.apple.com/us/app/prince-george-transit/id558331296?mt=8")!
-        private let mailSubject = "Check out 'PG Transit' on App Store!"
-        private let bugMailSubject = "PG Transit Report"
-        private let developerEmail = "yingmu52@gmail.com"
-          
+    // Add your other apps here
+    private let otherApps = [
+         "https://apps.apple.com/ca/app/marble-tilt/id1599878471",
+         "https://apps.apple.com/ca/app/winterland-scratcher/id1054185804",
+         "https://apps.apple.com/ca/app/itripoley/id1586541611"
+     ]
+              
     var body: some View {
         NavigationView {
             List {
-                Section(header: Text("Prince George Transit")) {
+                Section(header: Text("Marble Tilt").font(.headline)) {
                     Button("Share via SMS") {
                         showingMessage = true
                     }
-                    Button("Send Email") {
+                    Button("Share on Social") {
+                        showActivityView = true
+                    }
+                    Button("Email Feedback") {
                         showingMail = true
                     }
                 }
                 
-                Section(header: Text("Invite to PG Transit")) {
-                    Button("Open App Store") {
-                        UIApplication.shared.open(shareURL)
-                    }
-                }
-                
-                Section(header: Text("Feedback")) {
-                    Button("Send Bug Report Email") {
-                        showingMail = true
+                Section(header: Text("Check out more apps created by us").font(.headline)) {
+                    Button("Open App Store (Random)") {
+                        if let randomApp = otherApps.randomElement(),
+                           let url = URL(string: randomApp) {
+                            UIApplication.shared.open(url)
+                        }
                     }
                 }
             }
@@ -94,9 +84,17 @@ struct InfoView: View {
                     Text("This device cannot send email.")
                 }
             }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
         }
     }
 }
+
 struct ActivityView: UIViewControllerRepresentable {
     let activityItems: [Any]
     
@@ -105,3 +103,4 @@ struct ActivityView: UIViewControllerRepresentable {
     }
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
 }
+
